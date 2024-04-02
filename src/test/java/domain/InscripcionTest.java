cb1993351a4bb04b0ea5844107cc62b900e7ee89
Nicholas Sher <nsher@frba.utn.edu.ar>
2024-04-01 21:00:56 -0300

    corrijo error en los test

--- src/test/java/domain/InscripcionTest.java
+++ src/test/java/domain/InscripcionTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class InscripcionTest {
@@ -13,7 +14,7 @@ public class InscripcionTest {
         Inscripcion inscripcion = new Inscripcion(nico);
         inscripcion.agregarMaterias(algoritmos);
 
-        inscripcion.aprobada();
+        Assert.assertTrue(inscripcion.aprobada());
     }
 
     @Test
@@ -29,7 +30,7 @@ public class InscripcionTest {
         Inscripcion inscripcion = new Inscripcion(nico);
         inscripcion.agregarMaterias(paradigmas);
 
-        inscripcion.aprobada();
+        Assert.assertTrue(inscripcion.aprobada());
     }
 
     @Test
@@ -66,6 +67,44 @@ public class InscripcionTest {
         Inscripcion inscripcion = new Inscripcion(nico);
         inscripcion.agregarMaterias(sistemas, sintaxis, paradigmas, probabilidad, representacion, ingles, fisica, am2);
 
-        inscripcion.aprobada();
+        Assert.assertTrue(inscripcion.aprobada());
     }
+
+    @Test
+    public void noDeberiaInscribirSegundoAnio(){
+        Alumno nico = new Alumno("nicolas", "202.202.2");
+
+        Materia quimica = new Materia("quimica");
+        Materia discreta = new Materia("matematica discreta");
+        Materia algoritmos = new Materia("algoritmos y estructuras de datos");
+        Materia arquitectura = new Materia("arquitectura de computadoras");
+        Materia sYo = new Materia("sistemas y organizaciones");
+        Materia algebra = new Materia("algebra y geometria analitica");
+        Materia am1 = new Materia("analisis matematico 1");
+        Materia sociedad = new Materia("ingenieria y sociedad");
+
+
+        nico.agregarMaterias(quimica, discreta, algoritmos, arquitectura, sYo, sociedad, am1);
+
+        Materia sistemas = new Materia("analisis de sistemas");
+        Materia sintaxis = new Materia("sintaxis y semantica de los lenguajes");
+        Materia paradigmas = new Materia("paradigmas de programacion");
+        Materia probabilidad = new Materia("probabilidad y estadistica");
+        Materia representacion = new Materia("sistemas de representacion");
+        Materia ingles = new Materia("ingles tecnico 1");
+        Materia fisica = new Materia("fisica 1");
+        Materia am2 = new Materia("analisis matematico 2");
+
+        sistemas.agregarCorrelativas(algoritmos, sYo);
+        sistemas.agregarCorrelativas(algoritmos);
+        paradigmas.agregarCorrelativas(algoritmos,discreta);
+        probabilidad.agregarCorrelativas(algebra, am1);
+        am2.agregarCorrelativas(am1);
+
+        Inscripcion inscripcion = new Inscripcion(nico);
+        inscripcion.agregarMaterias(sistemas, sintaxis, paradigmas, probabilidad, representacion, ingles, fisica, am2);
+
+        Assert.assertFalse(inscripcion.aprobada());
+    }
+
 }
